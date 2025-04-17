9b0e67639e6feb487e218769e100a0ebbb601988
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-17 15:05:52 +0200

    [UPD] Initial app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+// devo importare express
+const express = require("express")
+
+//inizializzo express nella variabile app
+const app = express()
+
+//definisco la porta 
+const port = 3000
+
+
+
+//metto in ascolto il server
+app.listen(port,()=>{
+    console.log(`Server in ascolto alla porta ${port}`)
+})
