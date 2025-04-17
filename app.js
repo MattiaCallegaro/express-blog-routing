5be3352ec6a030db404c1b695cc61b56c0d6c432
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-17 16:25:12 +0200

    [FIX] Fix image

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get("/", (req, res) =>{
     res.send("Il mio blog")
 })
 
+//utilizzo asset statici
+app.use(express.static('public'))
+
 //definisco la rotta post
 app.get("/post", (req, res) =>{
     const post = [
@@ -20,7 +23,7 @@ app.get("/post", (req, res) =>{
           title: "Ciambellone",
           content:
             "Sarà che una volta le cose erano più semplici, ma erano anche molto buone. Come le crostate, i biscotti o il ciambellone che la nonna preparava anche all'ultimo sapendo che sareste passati per la merenda: uova, zucchero e farina. Niente di più basic ma che tra le sue mani, mescolando e infornando, diventava una delle prelibatezze per accompagnare il succo di frutta al pomeriggio o il latte e caffè al mattino. Ecco la nostra ricetta del ciambellone a quale atmosfera si ispira, quella di casa e genuinità: con una manciata di scorze di limone o di arancia e una spolverata di zucchero a velo renderete questa soffice delizia profumata e invitante. E per una volta sarà la nonna a farvi i complimenti per aver preparato un morbido ciambellone, così buono che non passa mai di moda!",
-          image: "/imgs/posts/ciambellone.avif",
+          image: "/imgs/posts/ciambellone.jpeg",
           tags: ["Dolci", "Torte", "Ricette vegetariane", "Ricette al forno"],
         },
         {
@@ -31,7 +34,7 @@ app.get("/post", (req, res) =>{
                     Cracker di lupini
                     Cracker allo zafferano
                     Cracker ai semi`,
-          image: "cracker_barbabietola.avif",
+          image: "/imgs/posts/cracker_barbabietola.jpeg",
           tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
         },
         {
@@ -41,21 +44,21 @@ app.get("/post", (req, res) =>{
                   Pasta con barbabietola e pecorino
                   Gnocchi di barbabietola
                   Tagliatelle alla barbabietola con asparagi`,
-          image: "/imgs/posts/pasta_barbabietola.avif",
+          image: "/imgs/posts/pasta_barbabietola.jpeg",
           tags: ["Primi piatti", "Ricette vegetariane"],
         },
         {
           id: 4,
           title: "Pane fritto dolce",
           content: `Il pane fritto dolce è la versione più antica dell'odierno french toast! Una deliziosa ricetta antispreco che le nonne preparavano ai bambini per merenda, utilizzando gli ingredienti che si avevano sempre a disposizione in casa: pane raffermo, uova, latte e zucchero, che noi abbiamo deciso di aromatizzare con un pizzico di cannella. Facile e veloce da realizzare, il pane fritto dolce vi riporterà con la mente ai sapori dell'infanzia… gustatelo da solo o accompagnatelo con frutta fresca e yogurt per uno spuntino tanto goloso quanto genuino!`,
-          image: "/imgs/posts/pane_fritto_dolce.avif",
+          image: "/imgs/posts/pane_fritto_dolce.jpeg",
           tags: ["Dolci", "Dolci veloci", "Ricette veloci", "Ricette vegetariane"],
         },
         {
           id: 5,
           title: "Torta paesana",
           content: `La torta paesana è un dolce di origine lombarda e precisamente della Brianza, la zona compresa tra la provincia a nord di Milano e il lago di Lecco-Como. E' un dolce di origine contadina, dalle infinite varianti, ma realizzata principalmente con pane raffermo bagnato nel latte. E' infatti conosciuta anche come torta di pane o, in dialetto locale, “michelacc” ovvero mica e lac (pane e latte). A seconda dei gusti e delle disponibilità del momento, al pane ammollato ogni famiglia univa ingredienti diversi, chi l'uvetta o chi i pinoli ad esempio. Noi vi presentiamo la nostra versione con l'aggiunta di cacao e amaretti: perfetta da gustare per una merenda dal sapore rustico, la torta paesana è un perfetto dolce di recupero quando si ha del pane avanzato… ed è ancora più buona il giorno dopo!`,
-          image: "/imgs/posts/torta_paesana.avif",
+          image: "/imgs/posts/torta_paesana.jpeg",
           tags: ["Dolci", "Dolci al cioccolato", "Torte", "Ricette vegetariane", "Ricette al forno"],
         },
       ];
@@ -63,7 +66,7 @@ app.get("/post", (req, res) =>{
       res.json(post)
 
       //sevre per esportare posts
-      module.exports = posts;
+      module.exports = post;
 })
 
 //importazione rotte
